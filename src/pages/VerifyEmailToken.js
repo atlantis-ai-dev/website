58e78947486fc4c0e97fb9c1fe20efca431a855f
husaab <husseinsaabsrs@gmail.com>
2025-04-22 20:34:16 -0400

    import useref

--- src/pages/VerifyEmailToken.js
+++ src/pages/VerifyEmailToken.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSearchParams, useNavigate } from 'react-router-dom';
 import NavBar from '../components/Navbar/NavBar';
 import Footer from '../components/Footer';
